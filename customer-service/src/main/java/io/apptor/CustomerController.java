3ad7402657129afbe50d2f54040698d5e0229eef
Rao Chejarla <raochejarla@TestAccounts-MacBook-Pro.local>
2021-10-26 10:41:34 -0400

    added product mgmt dependency

--- customer-service/src/main/java/io/apptor/CustomerController.java
+++ customer-service/src/main/java/io/apptor/CustomerController.java
@@ -1,5 +1,7 @@
 package io.apptor;
 
+import io.apptor.productmgmt.sdk.clients.IRoleClient;
+import io.apptor.productmgmt.sdk.models.Role;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.*;
 import io.micronaut.scheduling.TaskExecutors;
@@ -18,12 +20,15 @@ public class CustomerController {
 
     private CustomerRepository repository;
     private OrderClient orderClient;
+    private IRoleClient roleClient;
 
-    public CustomerController(CustomerRepository repository, OrderClient orderClient)
+
+    public CustomerController(CustomerRepository repository, OrderClient orderClient, IRoleClient roleClient)
     {
         System.out.println("CustomerController");
         this.repository = repository;
         this.orderClient = orderClient;
+        this.roleClient = roleClient;
     }
     @Get("/{id}")
     public Customer findById(int id) {
@@ -46,5 +51,15 @@ public class CustomerController {
 
         Customer customer =  this.repository.findById(id);
         return customer;
+    }
+    @Get("/product/{id}")
+    public List<Role> findRoles(long id) {
+
+        LOGGER.debug("In CustomerController.findRoles");
+
+        String accessToken = "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyYW8uY2hlamFybGFAZXhwZWVkc29mdHdhcmUuY29tIiwibGFzdE5hbWUiOiJDaGVqYXJsYSIsImNsaWVudElkIjo1MTcsInByb2R1Y3RJZCI6MCwicm9sZXMiOltdLCJpc3MiOiJ1c2VybWdtdC1hcGkiLCJ1c2VyTmFtZSI6InJhby5jaGVqYXJsYUBleHBlZWRzb2Z0d2FyZS5jb20iLCJ1c2VySWQiOjYyOSwiZmlyc3ROYW1lIjoiUmFvIiwibmJmIjoxNjM1MTc5NjIzLCJleHAiOjE2MzYwNDM2MjMsImlhdCI6MTYzNTE3OTYyM30.cd3bALJOloLahKtiTvn5I0I-uiIg_ds9JzXKYkpIvKc";
+        List<Role> lstRoles = roleClient.getDefaultRolesByProductId(id, accessToken);
+
+        return lstRoles;
     }
 }
